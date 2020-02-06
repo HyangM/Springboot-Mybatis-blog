60ccd7517a997856c3fff198baa571f13bd37674
LHM <gidal5007@naver.com>
2020-02-06 09:44:12 +0900

    스프링부트 블로그만들기 개발환경 세팅 완료

---
+++ src/main/java/com/hm/springboot/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.hm.springboot.repository;
+
+public class UserRepository {
+	// 전체보기
+//	List<User> findAll();
+	// 상세보기
+//	Mem findById(int id);
+//	// 회원가입
+//	int save(RequestMemJoinDto mem);
+//	// 회원수정
+//	int update(RequestMemUpdateDto mem);
+//	// 회원삭제
+//	int delete(int id);
+
+}
